e82f5858b2912033ce2676a7ad24c6b21ca4b3a0
Sanskruti Kothari <sanskrutikothari1824@gmail.com>
2026-07-20 18:21:13 +0530

    Fix isUserHost evaluation in Result pages to correctly display Host UI and controls

--- client/src/pages/FinalResult.jsx
+++ client/src/pages/FinalResult.jsx
@@ -79,7 +79,7 @@ export default function FinalResult() {
       if (userStr) user = JSON.parse(userStr);
     } catch (e) {}
 
-    if (game && user && game.hostId === user.id) {
+    if (game && user && (game.hostId === user.id || game.hostId?._id === user.id)) {
       setIsHost(true);
     } else {
       setIsHost(!localPlayerName && !!hostToken);
